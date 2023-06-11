f97d3c78accc6b3088855eb7b7273c070424dbc3
Create Lab <67844237+ando818@users.noreply.github.com>
2023-06-11 15:15:21 -0700

    signup form

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -2,7 +2,45 @@
 	import { goto } from '$app/navigation';
 
 	export let data;
-
 </script>
 
-<ion-content />
+<ion-content>
+	<div class='container'>
+	<ion-list lines="full" class="ion-no-margin ion-no-padding">
+		<ion-header translucent={true}>
+			<ion-toolbar>
+			   <ion-buttons slot="start">
+				  <ion-menu-button />
+			   </ion-buttons>
+			   <ion-buttons slot="end">
+			   </ion-buttons>
+			   <ion-title>Create Account</ion-title>
+			</ion-toolbar>
+		 </ion-header>
+		<ion-item>
+			<ion-input label="Username" label-placement="floating" name="username" />
+		</ion-item>
+		<ion-item>
+			<ion-input label="Email" label-placement="floating" name="email" />
+		</ion-item>
+		<ion-item>
+			<ion-input label="Password" label-placement="floating" name="password" />
+		</ion-item>
+		<ion-item>
+			<ion-input label="Verify Password" label-placement="floating" name="password-verify" />
+		</ion-item>
+		<div class="ion-padding">
+			<ion-button expand="block" type="submit" class="ion-no-margin"> Create account </ion-button>
+		 </div>
+		
+	</ion-list>
+</div>
+</ion-content>
+<style>
+	.container {
+		width:50%;
+		margin:auto;
+		margin-top:5%;
+	}
+
+</style>
