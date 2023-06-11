e9678f4aeb2c17b15cbcc22fe69a1bdc06973d77
Create Lab <67844237+ando818@users.noreply.github.com>
2023-06-11 14:24:27 -0700

    somewhat working nested comments

---
+++ src/lib/components/Comment.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { comment } from 'svelte/internal';
+
+	export let commentTree;
+    export let level;
+
+	console.log(commentTree);
+</script>
+
+<div class={level > 0 ? "indent": ""}>
+	{#if commentTree.children}
+		<ion-list>
+			{#each commentTree.children as tree}
+				<ion-item>
+					{tree.comment.comment.content}
+				</ion-item>
+				{#if tree.children.length > 0}
+					<svelte:self commentTree={tree} level={level+1}/>
+				{/if}
+			{/each}
+		</ion-list>
+	{/if}
+</div>
+
+<style>
+	.indent {
+		margin-inline-start: 1rem;
+	}
+	.comment.lines {
+		position: relative;
+		padding-inline-start: 1rem;
+	}
+</style>
--- src/routes/post/[id]/+page.svelte
+++ src/routes/post/[id]/+page.svelte
@@ -2,40 +2,44 @@
 	import Post from '$lib/components/Post.svelte';
 	import { currentPost } from '$lib/store';
 	import { IonPage } from 'ionic-svelte';
-	import {onMount} from 'svelte'
+	import { onMount } from 'svelte';
+	import Comment  from '$lib/components/Comment.svelte'
 	export let data;
 
 
 	let commentTree = {
 		id: 0,
 		children: []
-	}
+	};
 
-
-	onMount(() => {
-		console.log(data)
-		let commentsSorted = data.comments.sort((d) => d.comment.path).reverse()
-		console.log(commentsSorted)
-		for (let i=0; i<data.comments.length; i++) {
-			let comment = data.comments[i]
+	function convertToTree(comments) {
+		let commentsSorted = comments.sort((d) => d.comment.path).reverse();
+		for (let i = 0; i < commentsSorted.length; i++) {
+			let comment = data.comments[i];
 			let path = comment.comment.path;
-			let pathSplit = path.split('.')
+			let pathSplit = path.split('.');
 			let root = commentTree;
 
-			for (let j=1;j<pathSplit.length; j++) {
-				let index = pathSplit[j]
-				if(!root.children[index]) {
+			for (let j = 1; j < pathSplit.length; j++) {
+				let id = pathSplit[j];
+				let index = root.children.findIndex((c) => c.id == id);
+				if (index == -1) {
 					root.children.push({
 						id: comment.comment.id,
-						comment
-					})
+						comment,
+						children: []
+					});
+				} else {
+					root = root.children[index];
 				}
-				//root = commentTree[]
 			}
 		}
+	}
+	onMount(() => {
+		convertToTree(data.comments);
+		commentTree = commentTree;
 		console.log(commentTree)
-	})	
-
+	});
 </script>
 
 <IonPage>
@@ -52,18 +56,10 @@
 						placeholder={$currentPost.post.body}
 					/>
 				</div>
-				<div id='comments'>
-					<ion-list>
-						{#each data.comments as comment}
-						<ion-item>
-							{comment.comment.content}
-
-						</ion-item>
-						{/each}
-					</ion-list>
+				<div id="comments">
+					<Comment bind:commentTree={commentTree} level={0}></Comment>
 				</div>
 			</div>
-
 
 			<div id="right-bar">hi</div>
 		</div>
