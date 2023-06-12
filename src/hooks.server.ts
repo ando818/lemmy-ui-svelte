10ea8ec629f99d4526716e643b6a11a7ab77944b
Create Lab <67844237+ando818@users.noreply.github.com>
2023-06-11 19:38:29 -0700

    one more

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -18,7 +18,7 @@ export const handle: Handle = async ({ resolve, event }) => {
   }
 
   const response = await resolve(event);
-  if (event.url.pathname.startsWith('/api')) {
+  if (event.url.pathname.startsWith('/apus')) {
     response.headers.append('Access-Control-Allow-Origin', `*`);
   }
   return response;
