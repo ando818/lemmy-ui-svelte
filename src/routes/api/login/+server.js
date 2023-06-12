e3941a583a9fd31850d5b0e10a0dd539200fc5b6
Create Lab <67844237+ando818@users.noreply.github.com>
2023-06-11 18:59:57 -0700

    route

--- src/routes/api/login/+server.js
+++ src/routes/api/login/+server.js
@@ -9,7 +9,7 @@ export async function POST({ cookies, request }) {
         password: data.password,
     }));
 
-    let resp = await fetch("https://retarded.dev/api/v3/user/login", {
+    let resp = await fetch("/api/v3/user/login", {
         method: "POST",
         headers: {
             "content-type": "application/json",
