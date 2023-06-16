d2ae5a0de19df8a01f1f47a9da00b72703fe9a14
ando818 <67844237+ando818@users.noreply.github.com>
2023-06-16 09:43:57 -0700

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,15 +2,9 @@ export const load = async ({ cookies }) => {
 
     let resp = await fetch("https://lemmy.world/api/v3/community/list")
     let json = await resp.json()
-    console.log(json)
-    
-
 	return {
-		/**
-		 * The player's guessed words so far
-		 */
 		communities: json.communities,
-        posts: await getPosts()
+       		 posts: await getPosts()
 	};
 };
 
