1d2de5aeec13ba56736a1e0c24feb0dd8b3c5d49
Create Lab <67844237+ando818@users.noreply.github.com>
2023-06-15 23:14:06 -0700

    iamge

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -5,8 +5,7 @@
 	import { Chatbox } from 'svelte-ionicons';
 	import { currentPost } from '$lib/store.js';
 	function parsePostType(post) {
-		if (post.post.url?.includes('.jpg') || 
-		post.post.url?.includes('.png') ) {
+		if (post.post.url?.includes('.jpg') || post.post.url?.includes('.png')) {
 			return 'image';
 		} else if (post.post.thumbnail_url) {
 			return 'video';
@@ -33,39 +32,40 @@
 	}
 </script>
 
-<ion-item class='item'>
-	<div></div>
+<ion-item class="item">
+	<div class='image-container'>
 	{#if parsePostType(post) == 'video'}
-		<ion-img src={post.post.thumbnail_url} style="width:10vw; height: 10vh; padding-right:10px" />
+		<ion-img src={post.post.thumbnail_url} style="width:100%;  object-fit: cover; padding-right:10px" />
 	{:else if parsePostType(post) == 'image'}
 		<a href={post.post.url}
-			><ion-img src={post.post.url} style="width:10vw; height: 10vh; padding-right:10px" /></a
+			><ion-img src={post.post.url} style="width:100%; object-fit: cover; padding-right:10px" /></a
 		>
 	{:else}
 		<div
-			style="width: 10vw; height: 10vh; display:flex; align-items: center; padding-right: 10px; justify-content: center"
+			style="width: 100%; height: 10vh; display:flex; align-items: center; padding-right: 10px; justify-content: center"
 		>
 			<Chatbox size="25" />
 		</div>
 	{/if}
+	</div>
 	<ion-label>
 		<div class="post mobile">
-		<div
-			on:click={() => {
-				if (post.post.url) {
-					goTo(post.post.url);
-				} else {
-					goToPost(post);
-				}
-			}}
-		>
-			<h2 class='post-title'>{post.post.name}</h2>
+			<div
+				on:click={() => {
+					if (post.post.url) {
+						goTo(post.post.url);
+					} else {
+						goToPost(post);
+					}
+				}}
+			>
+				<h2 class="post-title">{post.post.name}</h2>
+			</div>
+			<h4><a>@{getDisplayName(post)}</a> to {post.community.title}</h4>
+			<div on:click={() => goToPost(post)}>
+				<a><h3>{post.counts.comments} comments</h3></a>
+			</div>
 		</div>
-		<h4><a>@{getDisplayName(post)}</a> to {post.community.title}</h4>
-		<div on:click={() => goToPost(post)}>
-			<a><h3>{post.counts.comments} comments</h3></a>
-		</div>
-	</div>
 	</ion-label>
 </ion-item>
 
@@ -74,11 +74,19 @@
 		height: fit-content;
 	}
 
+	.image-container {
+		width:100px;
+	
+	}
+
 	.post-title {
 		word-wrap: break-word;
 		white-space: normal;
 	}
 	@media (max-width: 767px) {
+		.image-container {
+			width:20vw;
+		}
 		.mobile h2 {
 			font-size: 12px;
 		}
